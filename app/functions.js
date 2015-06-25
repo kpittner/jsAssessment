42977e8d78443057284d4ebb6bfe46b067eb7ad8
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-06-25 11:43:56 -0400

    logicalOperators and part of functions

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction : function(str) {
-
+    return function(arg) {
+      return str + ', ' + arg;
+    };
   },
 
   makeClosures : function(arr, fn) {
-
+    
   },
 
   partial : function(fn, str1, str2) {
