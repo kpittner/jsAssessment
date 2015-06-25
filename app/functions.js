8cf8aca6c54a2f63e73e5cce61df5cf2fa868402
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-06-25 13:02:52 -0400

    objects

--- app/functions.js
+++ app/functions.js
@@ -16,19 +16,38 @@ exports.functionsAnswers = {
   },
 
   makeClosures : function(arr, fn) {
-    
+    var ret = [];
+
+    var makeFn = function(val) {
+      return function() { return fn(val); };
+    }
+
+    for (var i = 0; i < arr.length; i++) {
+      ret.push(makeFn(arr[i]));
+    }
+
+    return ret;
   },
 
   partial : function(fn, str1, str2) {
-
+    return function(str3) {
+      return fn.call(null, str1, str2, str3);
+    };
   },
 
   useArguments : function() {
+    var sum = 0;
 
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+
+    return sum;
   },
 
   callIt : function(fn) {
-
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+      fn.apply(null, args);
   },
 
   partialUsingArguments : function(fn) {
