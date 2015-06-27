87a36a75df95f71256f5ecbd2d38e36a360a6062
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-06-27 15:22:18 -0400

    recursion

--- app/recursion.js
+++ app/recursion.js
@@ -9,20 +9,21 @@ exports.recursionAnswers = {
 
     function processDir(dir) {
       var i, len, file;
-      var file = dir.files;
+      var files = dir.files;
 
       dirs.push( dir.dir );
 
       for (i = 0, len = files.length; i < len; i++) {
         file = files[i];
-        if (typeof file === "string") {
-          if (!dirName ||dirs.indexOf(dirName) > -1 ) {
+        if (typeof file === 'string') {
+          if (!dirName || dirs.indexOf(dirName) > -1) {
             listOfFiles.push(files[i]);
           }
         } else {
           processDir(files[i]);
         }
       }
+
       dirs.pop();
     }
 
